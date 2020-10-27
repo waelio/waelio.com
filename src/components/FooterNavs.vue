d38e023ff1471a234ccb855743a71f136cd5b64f
Wael Wahbeh <wahbehw@gmail.com>
2020-10-27 15:58:26 +0200

    cleaner footer

--- src/components/FooterNavs.vue
+++ src/components/FooterNavs.vue
@@ -1,131 +1,161 @@
 <template>
-  <div class="row justify-around align-center q-px-0 q-mx-auto pt-0 q-pb-xs">
-    <div class="col-xs-6 col-sm-6 col-md-6 col-lg-4 col-xl-4">
-      <h4 class="q-my-sm width-100 border-white">Projects</h4>
-      <div class="col">
-        <q-btn
-          align="center"
-          flat
-          size="md"
-          stretch
-          to="/projects/picmymenu"
-          text-color="white"
-          label="Picmymenu"
-          :class="{'active':$route.params.projectName ==='picmymenu'}"
-        />
-      </div>
-      <div class="col-xs-6 col-sm-6 col-md-6 col-lg-4 col-xl-4">
-        <q-btn
-          align="center"
-          flat
-          size="md"
-          stretch
-          to="/projects/sudoku"
-          text-color="white"
-          label="Sudoku"
-          :class="{'active':$route.params.projectName ==='sudoku'}"
-        />
-      </div>
-      <div class="col-xs-6 col-sm-6 col-md-6 col-lg-4 col-xl-4">
-        <q-btn
-          align="center"
-          flat
-          size="md"
-          stretch
-          to="/projects/q2p"
-          text-color="white"
-          label="Q2P"
-          :class="{'active':$route.params.projectName ==='q2b'}"
-        />
-      </div>
-      <div class="col-xs-6 col-sm-6 col-md-6 col-lg-4 col-xl-4">
-        <q-btn
-          align="center"
-          flat
-          size="md"
-          stretch
-          to="/projects/picmymenu"
-          text-color="white"
-          label="Picmymenu"
-          :class="{'active':$route.params.projectName ==='picmymenu'}"
-        />
-      </div>
-    </div>
-    <div class="col-xs-6 col-sm-6 col-md-6 col-lg-4 col-xl-4">
-      <h4 class="q-my-sm text-center width-100 border-white">General</h4>
-      <div class="col">
-        <q-btn
-          align="center"
-          flat
-          size="md"
-          stretch
-          to="/privacy"
-          text-color="white"
-          label="Privacy"
-          :class="{'active': $route.name === 'privacy'}"
-        />
-      </div>
-      <div class="col">
-        <q-btn
-          align="center"
-          flat
-          size="md"
-          stretch
-          to="/terms"
-          text-color="white"
-          label="Terms"
-          :class="{'active': $route.name === 'terms'}"
-        />
-      </div>
-      <div class="col">
-        <q-btn
-          align="center"
-          flat
-          size="md"
-          stretch
-          to="/contact"
-          text-color="white"
-          label="Write me"
-          :class="{'active': $route.name === 'contact'}"
-        />
-      </div>
-      <div class="col">
-        <q-select
-          align="center"
-          class="text-center items-center pt-0 capitalize text-h5"
-          borderless
-          hide-dropdown-icon
-          item-aligned
-          label-color="white"
-          :label="$i18n.locale.name || 'English'"
-          hide-selected
-          v-model="$i18n.locale"
-          :options-value="item => item.value"
-          :option-label="item => item.name"
-          standout
-          :options="[
-            { value: 'en-us', name: 'English' },
-            { value: 'ar-ps', name: 'Arabic' },
-            { value: 'he-il', name: 'Hebrew' }
-          ]"
+  <div class="row justify-around align-center" id="footer-navs">
+    <div class="col-xs-3 col-sm-3 col-md-4 col-lg-3 col-xl-3">
+      <q-list>
+        <q-item-label
+          ><h4 class="q-my-sm text-center border-white">
+            Projects
+          </h4></q-item-label
         >
-        </q-select>
-      </div>
+        <q-item dense>
+          <q-item-section>
+            <q-btn
+              align="center"
+              flat
+              size="md"
+              stretch
+              to="/projects/picmymenu"
+              text-color="white"
+              label="Picmymenu"
+              :class="{ active: $route.params.projectName === 'picmymenu' }"
+            />
+          </q-item-section>
+        </q-item>
+        <q-item dense>
+          <q-item-section>
+            <q-btn
+              align="center"
+              flat
+              size="md"
+              stretch
+              to="/projects/sudoku"
+              text-color="white"
+              label="Sudoku"
+              :class="{ active: $route.params.projectName === 'sudoku' }"
+            />
+          </q-item-section>
+        </q-item>
+        <q-item dense>
+          <q-item-section>
+            <q-btn
+              align="center"
+              flat
+              size="md"
+              stretch
+              to="/projects/q2p"
+              text-color="white"
+              label="Q2P"
+              :class="{ active: $route.params.projectName === 'q2b' }"
+            />
+          </q-item-section>
+        </q-item>
+      </q-list>
     </div>
-    <div class="col-xs-6 col-sm-6 col-md-6 col-lg-4 col-xl-4">
-      <h4 class="q-my-sm text-center width-100 border-white">About Me</h4>
-      <div class="col">
-        <q-btn
-          align="center"
-          flat
-          size="md"
-          stretch
-          to="/timeline"
-          text-color="white"
-          label="Portfolio"
-          :class="{'active': $route.name === 'timeline'}"
-        />
-      </div>
+    <div class="col-xs-3 col-sm-3 col-md-4 col-lg-3 col-xl-3">
+      <q-list>
+        <q-item-label
+          ><h4 class="q-my-sm text-center border-white">
+            General
+          </h4></q-item-label
+        >
+        <q-item dense>
+          <q-item-section>
+            <q-btn
+              align="center"
+              flat
+              size="md"
+              stretch
+              to="/privacy"
+              text-color="white"
+              label="Privacy"
+              :class="{ active: $route.name === 'privacy' }"
+            />
+          </q-item-section>
+        </q-item>
+        <q-item dense>
+          <q-item-section>
+            <q-btn
+              align="center"
+              flat
+              size="md"
+              stretch
+              to="/terms"
+              text-color="white"
+              label="Terms"
+              :class="{ active: $route.name === 'terms' }"
+            />
+          </q-item-section>
+        </q-item>
+        <q-item dense>
+          <q-item-section>
+            <q-btn
+              align="center"
+              flat
+              size="md"
+              stretch
+              to="/contact"
+              text-color="white"
+              label="Write me"
+              :class="{ active: $route.name === 'contact' }"
+            />
+          </q-item-section>
+        </q-item>
+      </q-list>
+    </div>
+    <div class="col-xs-3 col-sm-3 col-md-4 col-lg-3 col-xl-3">
+      <q-list>
+          <q-item-label
+            ><h4 class="q-my-sm text-center border-white">
+              About Me
+            </h4></q-item-label
+          >
+        <q-item dense>
+          <q-item-section>
+            <q-btn
+              align="center"
+              flat
+              stretch
+              to="/timeline"
+              text-color="white"
+              label="Portfolio"
+              :class="{ active: $route.name === 'timeline' }"
+            />
+          </q-item-section>
+        </q-item>
+        <q-item dense>
+          <q-item-section>
+            <span class="text-center">Select Language:</span>
+            <span>
+            <q-select
+              id="special-select"
+              class="align-center text-center"
+              align="center"
+              borderless
+              hide-dropdown-icon
+              item-aligned
+              label-color="white"
+              :label="$i18n.locale.name || 'English'"
+              hide-selected
+              v-model="$i18n.locale"
+              :options-value="item => item.value"
+              :option-label="item => item.name"
+              standout
+              :options="[
+                { value: 'en-us', name: 'English' },
+                { value: 'ar-ps', name: 'Arabic' },
+                { value: 'he-il', name: 'Hebrew' }
+              ]"
+            >
+            <template v-slot:label>
+              <div class="text-center">
+                {{$i18n.locale.name || 'English'}}
+              </div>
+            </template>
+            </q-select>
+            </span>
+          </q-item-section>
+        </q-item>
+      </q-list>
     </div>
     <div class="col-12">
       <q-splitter v-model="splitterMode" horizontal />
@@ -150,6 +180,9 @@ export default {
   padding-top: 0;
 }
 .active {
-  background-color: $secondary
+  background-color: $secondary;
+}
+#footer-navs #special-select .q-field__label {
+  text-align: center!important;
 }
 </style>
--- src/pages/Index.vue
+++ src/pages/Index.vue
@@ -1,8 +1,38 @@
 <template>
-<q-page padding>
-  <q-img src='~assets/profile_pic.jpg' spinner-color="white" style="height: 140px; max-width: 150px" />
-  <p>Wael Wahbeh</p>
-</q-page>
+  <q-page padding>
+    <q-img
+      src="~assets/profile_pic.jpg"
+      spinner-color="white"
+      style="height: 140px; max-width: 150px"
+    />
+    <p class="text-bold">Wael Wahbeh</p>
+    <p class="text-justify q-px-xs text-weight-medium">
+      I am a diverse/eclecic professional in the IT world, with over 22 years of
+      renowned success.
+    </p>
+    <p class="text-justify q-px-xs text-weight-medium">
+      When I leader and a mentor to a company of professionals, We set the bar
+      to the highest level in the market. The success was not due to my
+      understanding of the product, but by my interpersonal skills, problem
+      solving skills and my infinite capability to acquire knowledge.
+    </p>
+    <p class="text-justify q-px-xs text-weight-medium">
+      For a decade, I have assumed numerous executive positions. I am avid about
+      technological advancements and I love R&D. I have provided solutions such
+      as CRM, ERP, Inventory Manager and Accounting Managers –some of which are
+      being used today - for numerous prominent clients such Nike, PGA, NBA and
+      other well-established businesses.
+    </p>
+    <p class="text-justify q-px-xs text-weight-medium">
+      I am multilingual English, Arabic and Hebrew speaker, which are my native
+      languages, however, I can speak basic Russian as well
+    </p>
+    <p class="text-justify q-px-xs text-weight-medium">
+      Whenever possible, I am semi-professional aerial/sports photographer and
+      video producer.
+    </p>
+    <router-link to="timeline" class="text-bold">My Programming timeline</router-link>
+  </q-page>
 </template>
 <script>
 export default {
