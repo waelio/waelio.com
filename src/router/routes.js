c78d55cc2d12f40e89470454526bd3abf1b19806
Wael Wahbeh <wahbehw@gmail.com>
2020-10-30 20:21:28 +0200

    Route cleaning

--- src/router/routes.js
+++ src/router/routes.js
@@ -1,4 +1,3 @@
-
 const routes = [
   {
     path: '/',
@@ -6,7 +5,7 @@ const routes = [
     children: [
       { path: '', component: () => import('pages/Index.vue') },
       {
-        path: 'todo',
+        path: '/todo',
         name: 'todo',
         component: () => import('pages/Todo.vue'),
         meta: {
@@ -14,32 +13,33 @@ const routes = [
         }
       },
       {
-        path: 'privacy',
+        path: '/privacy',
         name: 'privacy',
         component: () => import('pages/Privacy')
       },
       {
-        path: 'contact',
+        path: '/contact',
         name: 'contact',
         component: () => import('pages/ContactUs')
       },
       {
-        path: 'terms',
+        path: '/terms',
         name: 'terms',
         component: () => import('pages/Terms')
       },
       {
-        path: 'timeline',
+        path: '/timeline',
         name: 'timeline',
         component: () => import('pages/TimeLine')
       },
       {
-        path: 'resume',
+        path: '/resume',
         name: 'resume',
         component: () => import('pages/Resume')
       },
+      // Projects
       {
-        path: 'projects',
+        path: '/projects',
         name: 'projects',
         component: () => import('pages/Projects/index'),
         children: [
@@ -50,18 +50,19 @@ const routes = [
           }
         ]
       },
+      // Apps
       {
-        path: 'apps',
+        path: '/apps',
         name: 'apps',
         component: () => import('pages/Apps/index'),
         children: [
           {
-            path: 'favsshuffler',
+            path: '/favsshuffler',
             name: 'favsshuffler',
             component: () => import('pages/Apps/FavsShuffler/index.vue'),
             children: [
               {
-                path: 'support',
+                path: '/support',
                 name: 'support',
                 component: () => import('pages/Apps/FavsShuffler/Support')
               }
@@ -70,42 +71,29 @@ const routes = [
         ]
       },
       {
-        path: 'login',
-        name: 'login',
-        component: () => import('pages/Auth.vue')
+        // Protected Routes
+        path: '/auth',
+        component: () => import('pages/Auth.vue'),
+        name: 'auth',
+        meta: {
+          requiresAuth: false
+        },
+        children: [
+          {
+            path: '/profile',
+            name: 'profile',
+            component: () => import('pages/Profile.vue'),
+            meta: {
+              requiresAuth: true
+            }
+          }
+        ]
+      },
+      {
+        path: '*',
+        component: () => import('pages/Error404.vue')
       }
     ]
-  },
-  { // Protected Routes
-    path: '/auth',
-    // beforeEnter: (to, from, next) => {
-    //   window.location = 'https://auth.waelio.com/login?client_id=2erlvso7q5ufgss3cl2c0acerv&response_type=code&scope=aws.cognito.signin.user.admin+email+openid+phone+profile&redirect_uri=https://waelio.com/'
-    // }
-    component: () => import('layouts/MyLayout.vue'),
-    children: [{
-      path: '',
-      name: 'auth',
-      component: () => import('pages/Auth.vue'),
-      meta: {
-        requiresAuth: false
-      }
-    }]
-  },
-  {
-    path: '/profile',
-    component: () => import('layouts/MyLayout.vue'),
-    children: [{
-      name: 'profile',
-      path: '',
-      component: () => import('pages/Profile.vue'),
-      meta: {
-        requiresAuth: true
-      }
-    }]
-  },
-  {
-    path: '*',
-    component: () => import('pages/Error404.vue')
   }
 ]
 
