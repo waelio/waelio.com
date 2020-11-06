0aab86de3ff40036edcb7a9243f9b7bad330c600
Wael wahbeh <waelio@users.noreply.github.com>
2020-11-06 22:59:34 +0200

    padding

--- src-pwa/notify.js
+++ src-pwa/notify.js
@@ -1,5 +1,4 @@
 export function notifyMe (notification) {
-
   if (!('Notification' in window)) {
     alert('This browser does not support desktop notification')
   } else if (Notification.permission === 'granted') {
