cb37aecf4dc2d6c68250fdf778e6017741fe3ee5
Wael wahbeh <waelio@users.noreply.github.com>
2020-11-06 22:58:51 +0200

    yalla!!!

--- src-pwa/notify.js
+++ src-pwa/notify.js
@@ -1,19 +1,11 @@
 export function notifyMe (notification) {
-  // Let's check if the browser supports notifications
-  if (!('Notification' in window)) {
-    console.log('This browser does not support desktop notification')
-  // eslint-disable-next-line brace-style
-  }
-  // Let's check whether notification permissions have already been granted
-  else if (Notification.permission === 'granted') {
-    // If it's okay let's create a notification
-    notification = new Notification('Welcome!')
-  }
 
-  // Otherwise, we need to ask the user for permission
-  else if (Notification.permission !== 'denied') {
+  if (!('Notification' in window)) {
+    alert('This browser does not support desktop notification')
+  } else if (Notification.permission === 'granted') {
+    notification = new Notification('Welcome!')
+  } else if (Notification.permission !== 'denied') {
     Notification.requestPermission().then(function (permission) {
-      // If the user accepts, let's create a notification
       if (permission === 'granted') {
         notification = new Notification('Hi there!')
       }
