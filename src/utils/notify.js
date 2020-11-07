1690ce2ded6f9b389e3d19bfbf427811fbfc8c97
Wael wahbeh <waelio@users.noreply.github.com>
2020-11-08 00:33:23 +0200

    Update notify.js

--- src/utils/notify.js
+++ src/utils/notify.js
@@ -1,7 +1,7 @@
 /* eslint-disable no-unused-vars */
 export function notifyMe (notification) {
   if (!('Notification' in window)) {
-    alert('This browser does not support desktop notification')
+    // alert('This browser does not support desktop notification')
   } else if (Notification.permission === 'granted') {
     notification = new Notification('Welcome to Waelio.com!')
   } else if (Notification.permission !== 'denied') {
