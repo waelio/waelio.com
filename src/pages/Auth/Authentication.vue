6f41ffa14b4ec476c3744402802f1e32fa9d5121
Wael wahbeh <waelio@users.noreply.github.com>
2020-11-09 21:25:44 +0200

    Federation auth

--- src/pages/Auth/Authentication.vue
+++ src/pages/Auth/Authentication.vue
@@ -7,6 +7,7 @@
       <q-btn color="info" label="Open UI" @click.prevent="openUI"></q-btn>
     </div>
     <pre>{{user}}</pre>
+    <pre>{{tempData}}</pre>
   </div>
 </template>
 <script>
@@ -20,15 +21,28 @@ export default {
   name: 'auth',
   props: ['isLoggedIn', 'signOut'],
   mounted () {
-    const { code, token } = this.$route.query
+    const { code } = this.$route.query
+    let token = window.location.hash.replace(/^#/, '') || false
     if (token) {
-      console.log('token', token)
+      token = this.QueryStringToJSON(token)
       this.handleResponse(token)
     }
     if (code) {
-      console.log('code', code)
+      // console.log('code', code)
     }
     Hub.listen('auth', ({ payload: { event, data } }) => {
+      switch (event) {
+        case 'signIn':
+          this.user = data
+          break
+        case 'signOut':
+          this.user = null
+          break
+        case 'customOAuthState':
+          this.customState = data
+      }
+    })
+    this.$AmplifyEventBus.$on('auth', ({ payload: { event, data } }) => {
       switch (event) {
         case 'signIn':
           this.user = data
@@ -47,15 +61,15 @@ export default {
       })
       // eslint-disable-next-line
       .catch(() => this.signedIn = false)
-    console.log(process.env)
   },
   data () {
     return {
+      tempData: null,
       user: null,
       isLoading: false,
       customState: null,
-      codeUrl: 'https://waelio.com/login?client_id=1ad0455rde18h4dorn5q96njvh&response_type=code&scope=aws.cognito.signin.user.admin+email+openid+phone+profile&redirect_uri=https://waelio.com/auth/process',
-      tokenUrl: 'https://waelio.com/login?client_id=1ad0455rde18h4dorn5q96njvh&response_type=token&scope=aws.cognito.signin.user.admin+email+openid+phone+profile&redirect_uri=https://waelio.com/auth/process'
+      codeUrl: 'https://auth.waelio.com/login?client_id=1ad0455rde18h4dorn5q96njvh&response_type=code&scope=aws.cognito.signin.user.admin+email+openid+phone+profile&redirect_uri=https://waelio.com/auth/process',
+      tokenUrl: 'https:/auth.waelio.com/login?client_id=1ad0455rde18h4dorn5q96njvh&response_type=token&scope=aws.cognito.signin.user.admin+email+openid+phone+profile&redirect_uri=https://waelio.com/auth/process'
     }
   },
   methods: {
@@ -85,25 +99,37 @@ export default {
         console.error(error)
       }
     },
+    QueryStringToJSON (query) {
+      var pairs = query.slice(1).split('&')
+      var result = {}
+      pairs.forEach(pair => {
+        pair = pair.split('=')
+        result[pair[0]] = decodeURIComponent(pair[1] || '')
+      })
+      return JSON.parse(JSON.stringify(result))
+    },
     async handleResponse (data) {
-      const { email, accessToken: token, expiresIn } = data
+      this.tempData = data
+      // eslint-disable-next-line camelcase
+      const { access_token: token, expires_in: expiresIn } = data
+      // console.log('access_token', token)
+      // console.log('expiresIn', expiresIn)
       const expiresAt = expiresIn * 1000 + new Date().getTime()
-      console.log(expiresAt)
-      const user = { email }
+      // console.log(expiresAt)
+      // const user = { email }
 
       this.isLoading = true
 
       try {
         const response = await Auth.federatedSignIn(
           'facebook',
-          { token, expiresAt },
-          user
+          { token, expiresAt }, { user: 'wahbehw@gmail.com' }
         )
         this.isLoading = false
         this.props.onLogin(response)
       } catch (e) {
         this.isLoading = false
-        this.handleError(e)
+        console.log('error', e)
       }
     }
   }
