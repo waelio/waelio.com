99e9c5cc4529cd5c8350125ccb7d3dabf0ab8159
Wael wahbeh <waelio@users.noreply.github.com>
2020-11-12 11:02:47 +0200

    disable gzip

--- quasar.conf.js
+++ quasar.conf.js
@@ -80,7 +80,7 @@ module.exports = function (ctx) {
       // rtl: false, // https://quasar.dev/options/rtl-support
       // preloadChunks: true,
       // showProgress: false,
-      gzip: true,
+      // gzip: true,
       // analyze: true,
 
       // Options below are automatically set depending on the env, set them if you want to override
