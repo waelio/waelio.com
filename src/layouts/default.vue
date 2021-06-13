3f17f48fd75b50c0130306217a8334ef83d4e8fc
Wael wahbeh <waelio@users.noreply.github.com>
2021-06-13 11:04:31 +0300

    no scroll

--- src/layouts/default.vue
+++ src/layouts/default.vue
@@ -6,7 +6,7 @@ const { t } = useI18n()
 <template>
   <main class="px-4 py-2 text-center text-gray-700 dark:text-gray-200">
     <Header />
-    <router-view class="overflow-y-scroll mx-auto" />
+    <router-view class="mx-auto" />
     <Footer />
     <div class="mt-2 mx-auto text-center opacity-25 text-sm">
       <router-link class="icon-btn mx-2" to="/" :title="t('button.home')">
