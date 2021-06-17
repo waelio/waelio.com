fb27f4fac6086c0ad139b574a39fe7db0b31d355
Wael wahbeh <waelio@users.noreply.github.com>
2021-06-17 16:03:35 +0300

    !working pinia

--- src/feathers.ts
+++ src/feathers.ts
@@ -1,7 +1,7 @@
 // src/feathers.ts
 import feathers from '@feathersjs/feathers'
 import socketio from '@feathersjs/socketio-client'
-import auth from '@feathersjs/authentication-client'
+// import auth from '@feathersjs/authentication-client'
 import io from 'socket.io-client'
 // eslint-disable-next-line @typescript-eslint/no-unused-vars
 import { iff, discard } from 'feathers-hooks-common'
@@ -12,8 +12,8 @@ const socket = io(`${import.meta.env.VITE_API_URL}`, { transports: ['websocket']
 
 const api = feathers()
   .configure(socketio(socket))
-  .configure(auth({
-    storage: !import.meta.env.SSR ? window.localStorage : '',
-  }))
+  // .configure(auth({
+  //   storage: !import.meta.env.SSR ? window.localStorage : '',
+  // }))
 
 export { api }
