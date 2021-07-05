5d9c798fc13efd5fc74ec195dc4ae1c0983ba703
Wael wahbeh <waelio@users.noreply.github.com>
2021-07-05 22:06:34 +0300

    more quasar layout

--- src/layouts/default.vue
+++ src/layouts/default.vue
@@ -3,7 +3,7 @@ import { ref, computed } from 'vue'
 import { isDark } from '~/logic'
 import { useQuasar } from 'quasar'
 const $q = useQuasar()
-const dimensions = ref($q.screen|| {height: 1000})
+const dimensions = ref($q ? $q.screen : {height: 1000})
 const draggingFab: boolean = ref(true)
 let fabPos: Ref<number[]>  = ref([18, 18])
  const moveFab = (ev): void => {
@@ -13,30 +13,32 @@ let fabPos: Ref<number[]>  = ref([18, 18])
 </script>
 <template>
   <main class="px-4 py-2 text-center text-gray-700 dark:text-gray-200">
-    <q-Layout  view="lHh Lpr lFf" container :style="`height:${dimensions.height*.96}px;`" >
-      <q-header reveal elevated>
-        <Header />
-      </q-header>
-    <q-page-container>
-      <router-view />
-      <q-page-scroller
-        position="bottom-right"
-        :scroll-offset="150"
-        :offset="fabPos"
-        :draggable="draggingFab"
-        v-touch-pan.prevent.mouse="moveFab"
-      >
-        <q-btn
-          fab          
-          :color=" isDark ? 'white' : 'dark'"
-          :text-color="!isDark ? 'white' : 'dark'"
-        ><q-icon name="keyboard_arrow_up" /></q-btn>
-      </q-page-scroller>
-    </q-page-container>
-    <q-footer>
-      <Footer />
-    </q-footer>
-  </q-layout>
+    <client-only>
+      <q-Layout  view="lHh Lpr lFf" container :style="`height:${dimensions.height*.96}px;`" >
+        <q-header reveal elevated>
+          <Header />
+        </q-header>
+        <q-page-container>
+          <router-view />
+          <q-page-scroller
+            position="bottom-right"
+            :scroll-offset="150"
+            :offset="fabPos"
+            :draggable="draggingFab"
+            v-touch-pan.prevent.mouse="moveFab"
+            >
+            <q-btn
+              fab          
+              :color=" isDark ? 'white' : 'dark'"
+              :text-color="!isDark ? 'white' : 'dark'"
+            ><q-icon name="keyboard_arrow_up" /></q-btn>
+          </q-page-scroller>
+        </q-page-container>
+        <q-footer>
+          <Footer />
+        </q-footer>
+      </q-layout>
+    </client-only>
   </main>
 </template>
 <style scoped>
