088520c4be294f7d1941c5a7cd81ad574717c456
Peace Marshal <88880103+peace2074@users.noreply.github.com>
2026-05-11 18:28:33 +0300

    feat: add @waelio/data to fallback package names list

--- src/app.tsx
+++ src/app.tsx
@@ -34,6 +34,7 @@ const NPM_MAINTAINER = "waelio";
 const FALLBACK_PACKAGE_NAMES = [
     "@waelio/agent",
     "@waelio/cli",
+    "@waelio/data",
     "@waelio/messaging",
     "@waelio/ustore",
     "@waelio/utils",
